4263acae308ec90422e9579a308016ce06885326
meomancer <meomancer@gmail.com>
2016-11-15 13:55:14 +0700

    init django project

---
+++ django_project/core/settings/contrib.py
@@ -0,0 +1,12 @@
+# coding=utf-8
+"""
+core.settings.contrib
+"""
+from .base import *  # noqa
+
+# Extra installed apps
+INSTALLED_APPS = (
+    'rest_framework',
+    'rest_framework_gis',
+    'django_countries',
+ ) + INSTALLED_APPS
